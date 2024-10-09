9468683ac612c554addcfddd74108c8fd9102903
siritami <103488094+siritami@users.noreply.github.com>
2024-10-09 12:32:38 +0700

    1

---
+++ src/build/ChromeXT.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ChromeXT
+source src/build/utils.sh
+
+dl_gh "JingMatrix" "LSPatch" "latest"
+get_apk "com.chrome.beta" "chrome-beta" "chrome-beta" "google-inc/chrome-beta/chrome-beta" "Bundle"
+java -jar lspatch.jar ./download/chrome-beta.apk -d -v -m ./release/ChromeXt-signed.apk --force
