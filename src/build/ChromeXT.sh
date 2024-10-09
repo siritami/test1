48ba64fbe3942ca8a2bd854d1bf558edb990e332
siritami <103488094+siritami@users.noreply.github.com>
2024-10-09 12:55:45 +0700

    Update ChromeXT.sh

--- src/build/ChromeXT.sh
+++ src/build/ChromeXT.sh
@@ -3,5 +3,5 @@
 source src/build/utils.sh
 
 dl_gh "JingMatrix" "LSPatch" "latest"
-get_apk "com.chrome.beta" "chrome-beta" "chrome-beta" "google-inc/chrome-beta/chrome-beta" "arm64-v8a"
+get_apk "com.chrome.beta" "chrome-beta" "chrome-beta" "google-inc/chrome-beta/chrome-beta" "BUNDLE</span>arm64-v8a"
 eval java -jar lspatch.jar ./download/chrome-beta.apk -d -v -m ./release/ChromeXt-signed.apk --force
