e57dda7d5b9ba7218c65279fcbccda9d09534815
siritami <103488094+siritami@users.noreply.github.com>
2024-10-10 13:01:53 +0700

    Update ChromeXT.sh

--- src/build/ChromeXT.sh
+++ src/build/ChromeXT.sh
@@ -6,4 +6,4 @@ wget -nv -O "./download/lspatch.jar" --header="User-Agent: Mozilla/5.0 (Linux; A
 wget -nv -O "./download/ChromeXt-signed.apk" --header="User-Agent: Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.231 Mobile Safari/537.36" "https://github.com/JingMatrix/ChromeXt/releases/download/v3.8.2/ChromeXt-signed.apk"
 get_apk "com.chrome.beta" "chrome-beta" "chrome-beta" "google-inc/chrome-beta/chrome-beta" "arm64-v8a"
 eval java -jar ./download/lspatch.jar ./download/chrome-beta.apk -d -v -m ./download/ChromeXt-signed.apk --force
-mv ./download/chrome-beta.apk ./release/chrome-beta-ChromeXt.apk
+mv ./chrome*.apk ./release/chrome-beta-ChromeXt.apk
