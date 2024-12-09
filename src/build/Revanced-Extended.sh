bea6a300428121b36d7589b4b55ac9ec5d135fd5
siritami <103488094+siritami@users.noreply.github.com>
2024-12-09 15:16:11 +0700

    1

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -4,7 +4,6 @@ source src/build/utils.sh
 dl_gh "revanced-patches" "inotia00" "latest"
 dl_gh "revanced-cli" "inotia00" "latest"
 
-revanced_dl
 # Patch YouTube:
 get_patches_key "youtube-revanced-extended"
 get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube"
