6b9cd0832ff2e9ae9f8f61356f524d9cdcd74ba6
siritami <103488094+siritami@users.noreply.github.com>
2024-12-12 22:12:56 +0700

    Update Revanced_Extended.py

--- src/build/Revanced_Extended.py
+++ src/build/Revanced_Extended.py
@@ -1,3 +1,5 @@
 from src.downloader.github import dl_gh
 
 dl_gh("revanced-patches", "ReVanced", "prerelease")
+dl_gh("revanced-patches", "ReVanced", "latest")
+dl_gh("revanced-patches", "ReVanced", "v5.4.0-dev.4")
