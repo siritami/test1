a410ad87b15d1e5058ba9da57ae2e077c8b84ed8
siritami <103488094+siritami@users.noreply.github.com>
2024-12-12 23:33:01 +0700

    Update Revanced_Extended.py

--- src/build/Revanced_Extended.py
+++ src/build/Revanced_Extended.py
@@ -1,16 +1,11 @@
 import sys
 import os
 
-# Add the src directory to the sys.path
 script_dir = os.path.dirname(__file__)
-src_dir = os.path.abspath(os.path.join(script_dir, '..', '..', 'src'))  # Going two levels up to src
+src_dir = os.path.abspath(os.path.join(script_dir, '..', '..', 'src'))
 sys.path.append(src_dir)
 
-# Now you can import the module
 from downloader.github import dl_gh
 
-# Your function calls
-dl_gh("revanced-patches", "ReVanced", "prerelease")
 dl_gh("revanced-patches", "ReVanced", "latest")
-dl_gh("revanced-patches", "ReVanced", "v5.4.0-dev.4")
 dl_gh("revanced-cli", "ReVanced", "latest")
