5e6febe40b4f3e657dd4dd3bc6e1ccdf15bcde2a
siritami <103488094+siritami@users.noreply.github.com>
2024-12-13 21:37:15 +0700

    Update Revanced_Extended.py

--- src/build/Revanced_Extended.py
+++ src/build/Revanced_Extended.py
@@ -1,9 +1,11 @@
 # Revanced_Extended.py
 import sys
 import os
+import time
 
 script_dir = os.path.dirname(__file__)
 src_dir = os.path.abspath(os.path.join(script_dir, '..', '..', 'src'))
+download_cli_path = os.path.join(script_dir, 'download_cli')
 sys.path.append(src_dir)
 
 from downloader.github import dl_gh
@@ -12,5 +14,6 @@ from downloader.apkmirror import download_apk
 dl_gh("revanced-patches", "ReVanced", "latest")
 dl_gh("revanced-cli", "ReVanced", "latest")
 dl_gh("revanced-integrations", "ReVanced", "latest")
+time.sleep(2)
 
 download_apk('com.google.android.youtube', 'https://www.apkmirror.com/apk/google-inc/youtube/youtube-19-47-41-release/', 'apk', 'youtube.apk', '', '', '', '')
