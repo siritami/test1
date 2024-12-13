bd80b810a2b175be2bc664693e13e8813790d0df
siritami <103488094+siritami@users.noreply.github.com>
2024-12-13 16:43:31 +0700

    Update Revanced_Extended.py

--- src/build/Revanced_Extended.py
+++ src/build/Revanced_Extended.py
@@ -5,11 +5,16 @@ script_dir = os.path.dirname(__file__)
 src_dir = os.path.abspath(os.path.join(script_dir, '..', '..', 'src'))
 sys.path.append(src_dir)
 
-from downloader.github import dl_gh
+from downloader.github import dl_gh, reset_cli_value
 from downloader.apkmirror import download_apk
 
+# Download necessary files first
 dl_gh("revanced-patches", "ReVanced", "latest")
 dl_gh("revanced-cli", "ReVanced", "latest")
 dl_gh("revanced-integrations", "ReVanced", "latest")
+
+# Now import the variables after they have been potentially set by dl_gh calls
 from downloader.github import major_cli_version, cli_exec, patches_exec, json_exec
+
+# Call download_apk after all necessary files have been downloaded
 download_apk("com.google.android.youtube", "https://www.apkmirror.com/apk/google-inc/youtube/", "apk", "youtube.apk", "", "", "nodpi", "Android 8.0+")
