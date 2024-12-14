14e93154c899ae59009d728bc1d07ddfec30accf
siritami <103488094+siritami@users.noreply.github.com>
2024-12-14 15:44:14 +0700

    1

--- src/build/Revanced_Extended.py
+++ src/build/Revanced_Extended.py
@@ -5,6 +5,12 @@ import requests
 import re
 import json
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 cli_exec = None
 patches_exec = None
