91fa9e9997762145e4bcd7a005e942985cecd3ed
siritami <103488094+siritami@users.noreply.github.com>
2025-03-26 17:50:48 +0700

    1

--- patch.sh
+++ patch.sh
@@ -12,20 +12,59 @@ APKTool="APKTool.jar"
 wget -q -O Uber.jar https://github.com/patrickfav/uber-apk-signer/releases/download/v1.3.0/uber-apk-signer-1.3.0.jar
 Uber="Uber.jar"
 
+# Colored output logs
+green_log() {
+    echo -e "\e[32m$1\e[0m"
+}
+red_log() {
+    echo -e "\e[31m$1\e[0m"
+}
 
+edit_AndroidManifest() {
+  local file="$1/AndroidManifest.xml"
+  sed -i 's/android:installLocation="internalOnly"//g' "$file"
+  sed -i 's/android:installLocation="auto"//g' "$file"
+  sed -i '0,/\(xmlns:android="http:\/\/schemas.android.com\/apk\/res\/android"\)/ s//\1 android:installLocation="preferExternal"/' "$file"
+}
 
+if [ $file_type == "APK" ]; then
+	file_ext_dl="apk"
+	split_apk="0"
+else
+	file_ext_dl="apkm"
+	split_apk="1"
+fi
 if [ $APK_URL == "upload" ]; then
-	wget -q -O app.apk https://github.com/$repository/releases/download/upload/app.apk
-	if [ -z "app.apk" ]; then
-		echo echo -e "\e[31mFailed to download file from your repository release, make sure your apk file you upload name 'app.apk' and tag release is 'upload'\e[0m"
+	wget -q -O app.$file_ext_dl https://github.com/$repository/releases/download/upload/app.$file_ext_dl
+	if [ -z "app.$file_ext_dl" ]; then
+		red_log "[-] Failed to download file from your repository release, make sure your apk file you upload name app.$file_ext_dl and tag release is 'upload'"
 		exit 0
 	fi
 else
-	wget -q -O app.apk $APK_URL
-	if [ -z "app.apk" ]; then
-		echo echo -e "\e[31mFailed to download file from your url given\e[0m"
+	wget -q -O app.$file_ext_dl $APK_URL
+	if [ -z "app.$file_ext_dl" ]; then
+		red_log "[-] Failed to download file from your url given"
 		exit 0
 	fi
 fi
-
-#
+if [ $split_apk == "1" ]; then
+	green_log "[+] Merging splits apk to standalone apk"
+	java -jar $APKEditor m -i app.$file_ext_dl
+	green_log "[+] Decrypting apk file"
+	java -jar $APKTool d app_merged.apk
+	green_log "[+] Changing AndroidManifest file"
+	edit_AndroidManifest "app_merged"
+	green_log "[+] Re-Compiling apk file"
+	java -jar $APKTool b app_merged -o app-sdcard.apk
+	green_log "[+] Signing apk file"
+	java -jar $Uber --apks app-sdcard.apk
+else
+	green_log "[+] Decrypting apk file"
+	java -jar $APKTool d app.apk
+	green_log "[+] Changing AndroidManifest file"
+	edit_AndroidManifest "app"
+	green_log "[+] Re-Compiling apk file"
+	java -jar $APKTool b app -o app-sdcard.apk
+	green_log "[+] Signing apk file"
+	java -jar $Uber --apks app-sdcard.apk
+fi
